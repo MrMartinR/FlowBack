8829c2144788fbd355f7b682520c9e67331fcbed
bainur <bainur_budi@yahoo.com>
2020-05-06 13:34:30 +0700

    ADD validation on registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   attr_accessor :tos
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validations
+
+  validates :username, presence: true, length: { minimum: 3 }
+  validates_uniqueness_of :username
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_163213) do
+ActiveRecord::Schema.define(version: 2020_05_06_055942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_05_01_163213) do
     t.index ["currency_id"], name: "index_users_on_currency_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
