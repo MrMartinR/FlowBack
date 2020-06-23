53e7267c054d1339e5437ee8110358f4fec62d55
midj2dev <67328205+midj2dev@users.noreply.github.com>
2020-06-23 17:37:50 -0400

    Update after_install.sh

--- .scripts/after_install.sh
+++ .scripts/after_install.sh
@@ -1,8 +1,11 @@
-sudo -s
-aws s3 sync --exact-timestamps s3://flowback-artifacts/ /app/
-docker run -v /app:/app -v /bundle_cache:/usr/local/bundle -p 80:80 -w /app ruby:2.6.5 gem install bundler:2.1.4
-docker run -v /app:/app -v /bundle_cache:/usr/local/bundle -p 80:80 -w /app ruby:2.6.5 bundle install
+#!/usr/bin/env bash
 
-docker container rm $(sudo docker container ls -aq)
+
+# Installation process
+sudo -s
+aws s3 cp --recursive s3://flowback-artifacts/ /app/
+docker container rm -f $(sudo docker container ls -aq)
+docker run -v /app:/app -v /bundle_cache:/usr/local/bundle -w /app ruby:2.6.5 gem install bundler:2.1.4
+docker run -v /app:/app -v /bundle_cache:/usr/local/bundle -w /app ruby:2.6.5 bundle install
 export SECRET_KEY_BASE="81a320f8221f3c1286191f5bc6ae13d7f5e048dbe627dd34b8985296d0e4375c4fe15f822f66d1ef1c27741510ccba9d33a07ab8cf507ae8c66fdc2df2c77fc6"
 docker run -d -v /app:/app -v /bundle_cache:/usr/local/bundle -e SECRET_KEY_BASE -p 80:80 -w /app ruby:2.6.5 bundle exec rails s -e production -p 80 -b 0.0.0.0
