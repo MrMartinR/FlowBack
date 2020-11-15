7ea8769b8a432c531d377eced05ee42bb1679ad9
Sajjad <sajjadmurtaza.nxb@gmail.com>
2020-11-15 09:29:21 +0100

    comment seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_212314) do
+ActiveRecord::Schema.define(version: 2020_06_16_024521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(version: 2020_11_14_212314) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.uuid "platform_id"
-    t.uuid "currency_id"
-    t.uuid "country_id"
+    t.json "currency_id"
+    t.json "country_id"
     t.index ["platform_id"], name: "index_accounts_on_platform_id"
   end
 
