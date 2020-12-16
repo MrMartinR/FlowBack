7280b3c1bb9583c1980b07c3de52ed064c34dd50
bafiam <stevebafiam@gmail.com>
2020-12-16 22:53:52 +0300

    add fetch all accounts and data functionality

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,42 +1,48 @@
 class Api::V1::AccountsController < Api::BaseController
   before_action :authenticate_api_v1_user!
   before_action :admin_or_contributor!
-  # before_action :set_account, only: [:show, :update, :destroy]
-
-  def index
-    if params[:page].blank?
-      @entities = Account.select('accounts.id,  accounts.name, accounts.category, accounts.icon').order('accounts.name asc')
-      res = json_response({ "entities": @entities, pages: @total_pages, page: 1 })
-    else
-      @entities = Account
-                  .select('accounts.id,  accounts.name, accounts.category, accounts.icon, accounts.currency_id, accounts.country_id')
-                  .order('accounts.name asc')
-                  .paginate(page: params[:page], per_page: params[:per_page])
-
-      @pages = Account.includes(:currency, :country).order('accounts.name asc').paginate(page: params[:page],
-                                                                                         per_page: params[:per_page]).total_pages
-
-      res = json_response({ "entities": @entities, pages: @pages, page: params[:page].to_i })
-    end
-    res
-  end
-
-  def show; end
+  before_action :set_account, only: [:show, :update, :destroy]
 
 =begin
-  The create allow an admin or a contributor to add an account
-  An account can have more that one country and more than one currecy
-  the counry and currecy comes in the form of arrays then added into the account
-  column
-  dublication is not an option so the use of arrays
+  This will get all the accounts data as well as corresponding relationship
+  
 =end
+  def index
+    @entities = []
+    Account.find_each do |account|
+      dt = Hash.new(0)
+      dt[:id] = account.id
+      dt[:contact] = account.contact
+      dt[:category] = account.category
+      dt[:platform] = account.platform
+      all_countries = []
+      account.country_id[0].each do |id|
+        all_countries << Country.find(id)
+      end
+      dt[:country] = all_countries
+      all_currencies = []
+
+      account.currency_id[0].each do |id|
+        all_currencies << Currency.find(id)
+      end
+      dt[:currency] = all_currencies
+      @entities << dt
+    end
+    json_response({ "accounts": @entities, pages: @total_pages, page: 1 })
+  end
+
+  #   The create allow an admin or a contributor to add an account
+  #   An account can have more that one country and more than one currecy
+  #   the counry and currecy comes in the form of arrays then added into the account
+  #   column
+  #   dublication is not an option so the use of arrays
   def create
-    @find_account = Account.find_by(name: account_params[:name])
+    @find_account = Account.find_by(contact_id: account_params[:contact_id])
     if @find_account.nil?
       @account = Account.new
-      @account.name = account_params[:name]
       @account.platform_id = account_params[:platform_id]
       @account.category = account_params[:category]
+      @account.contact_id = account_params[:contact_id]
       @account.currency_id = []
       @account.country_id = []
       @account.country_id.push(account_params[:country_ids])
@@ -48,7 +54,7 @@ class Api::V1::AccountsController < Api::BaseController
         render json: @account.errors, status: :unprocessable_entity
       end
     else
-      @find_account.errors.add( :show, message: 'Account already exist!')
+      @find_account.errors.add(:show, message: 'Account under the above contact already exist!')
       render json: @find_account.errors, status: :unprocessable_entity
     end
   end
@@ -72,6 +78,6 @@ class Api::V1::AccountsController < Api::BaseController
   end
 
   def account_params
-    params.require(:account).permit(:platform_id, :name, :category, currency_ids: [], country_ids: [])
+    params.require(:account).permit(:platform_id, :contact_id, :category, currency_ids: [], country_ids: [])
   end
 end
