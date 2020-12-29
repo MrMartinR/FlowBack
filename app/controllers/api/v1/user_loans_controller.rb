b8880b2a39c364d60655ce00838b9f2323bfc8a4
bafiam <stevebafiam@gmail.com>
2020-12-29 17:55:12 +0300

    remove pagination from index

--- app/controllers/api/v1/user_loans_controller.rb
+++ app/controllers/api/v1/user_loans_controller.rb
@@ -1,20 +1,13 @@
 class Api::V1::UserLoansController < Api::BaseController
   before_action :authenticate_api_v1_user!
   before_action :auth_admin!, only: :index_as_admin
-  before_action :set_user_loan, only: [:show, :update, :destroy]
+  before_action :set_user_loan, only: %i[show update destroy]
 
   def index
-    if params[:page].blank?
-      @user_loans = UserLoan.by_user(@user).order('created_at desc')
-    else
-      @user_loans = UserLoan.by_user(@user).order('created_at desc').paginate(page: params[:page], per_page: params[:per_page])
-      @total_pages = UserLoan.by_user(@user).order('created_at desc').paginate(page: params[:page], per_page: params[:per_page]).total_pages
-    end
+    @user_loans = UserLoan.by_user(@user).order('created_at desc')
   end
 
-  def show
-  end
-
+  def show; end
 
   def create
     @user_loan = @user.user_loans.new(user_loan_params)
@@ -22,7 +15,7 @@ class Api::V1::UserLoansController < Api::BaseController
     if @user_loan.save
       render :show, status: :created
     else
-      json_response({success:false, :message => @user_loan.errors},:unprocessable_entity)
+      json_response({ success: false, message: @user_loan.errors }, :unprocessable_entity)
     end
   end
 
@@ -30,7 +23,7 @@ class Api::V1::UserLoansController < Api::BaseController
     if @user_loan.update(user_loan_params)
       render :show, status: :ok
     else
-      json_response({success:false, :message => @user_loan.errors},:unprocessable_entity)
+      json_response({ success: false, message: @user_loan.errors }, :unprocessable_entity)
     end
   end
 
@@ -39,13 +32,15 @@ class Api::V1::UserLoansController < Api::BaseController
   end
 
   private
-    def set_user_loan
-      @user_loan = @user.user_loans.find(params[:id])
-    end
 
-    def user_loan_params
-      merge_params = { user_id: @user.id }
-      params.require(:user_loan).permit(:loan_id, :user_account_id, :slice_name, :market, :xirr, :investment_amount, :invest_mode, :position, :date_in, :date_out).merge(merge_params)
-    end
+  def set_user_loan
+    @user_loan = @user.user_loans.find(params[:id])
+  end
 
+  def user_loan_params
+    merge_params = { user_id: @user.id }
+    params.require(:user_loan).permit(:loan_id, :user_account_id, :slice_name, :market, :xirr, :investment_amount,
+                                      :invest_mode, :position, :date_in, :date_out).merge(merge_params)
+  end
 end
+
