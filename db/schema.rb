b81dbfaf549c27ca5b412ac5c9fe27d9d2788d5a
bafiam <stevebafiam@gmail.com>
2020-12-29 18:43:51 +0300

    migration to remove unneeded fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_142916) do
+ActiveRecord::Schema.define(version: 2020_12_29_153639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -293,9 +293,6 @@ ActiveRecord::Schema.define(version: 2020_12_29_142916) do
     t.string "internal_id"
     t.text "notes"
     t.integer "rating"
-    t.float "xirr"
-    t.float "total_loss"
-    t.float "air"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.uuid "created_by"
