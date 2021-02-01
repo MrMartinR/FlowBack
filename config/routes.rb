6d25f75bf5bfb4e2e6aa76a0c8b954461ee78b92
muhammad ebeid <muhammed.ebeid@gmail.com>
2021-02-01 16:55:32 +0200

    fix rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   get 'pages/index'
   get 'pages/dashboard'
- 
+
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth', :controllers => {
-          passwords: 'api/v1/passwords',
-          registrations: 'api/v1/registrations',
-          sessions: 'api/v1/sessions'
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        passwords: 'api/v1/passwords',
+        registrations: 'api/v1/registrations',
+        sessions: 'api/v1/sessions'
       }
 
       defaults format: :json do
@@ -17,8 +17,8 @@ Rails.application.routes.draw do
         resources :modify_currencies
         resources :countries
         resources :settings
-        resources :image_assets, :path => '/icons', only: [:index, :create, :update]
-        resources :users, only: [:index,:update]
+        resources :image_assets, path: '/icons', only: %i[index create update]
+        resources :users, only: %i[index update]
         resources :user_accounts do
           collection do
             get 'search'
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
             get 'search'
           end
         end
-        get '/platforms/:id/platform_originators', to: 'platform_originators#index_by_platform_id'        
+        get '/platforms/:id/platform_originators', to: 'platform_originators#index_by_platform_id'
         resources :loans
         resources :user_loans do
           collection do
@@ -44,16 +44,14 @@ Rails.application.routes.draw do
         end
 
         resources :transactions
-        
-
 
         match 'user_profile', to: 'users#user_profile', via: :get
-        match 'update_profile', to: 'users#update_profile', via: :post,constraints: { format: 'html' } 
+        match 'update_profile', to: 'users#update_profile', via: :post, constraints: { format: 'html' }
       end
     end
   end
   # routing errors
-  #match '*path', to: "errors#handle_root_not_found", via: [:get, :post, :put, :patch]
+  # match '*path', to: "errors#handle_root_not_found", via: [:get, :post, :put, :patch]
   get '*path', to: redirect('/'), constraints: lambda { |req|
     req.path.exclude? 'rails/active_storage'
   }
