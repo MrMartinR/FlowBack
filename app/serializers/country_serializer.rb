2c8e5f2a07849649620242b34276ceb94bfe36de
kishore <kishore@mallow-tech.com>
2021-02-02 16:05:30 +0530

    Added comments in serilizers file

--- app/serializers/country_serializer.rb
+++ app/serializers/country_serializer.rb
@@ -1,5 +1,7 @@
 class CountrySerializer
   include JSONAPI::Serializer
+  #sets the type of serializer
   set_type :country
+  #The attributes needed in response
   attributes :name, :iso_code, :continent
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,10 @@
 class UserSerializer
   include JSONAPI::Serializer
+  #sets the type of serializer
   set_type :user
+  #The attributes needed in response
   attributes :email, :username
+  #Serializer for associatations
   belongs_to :country
   belongs_to :currency
 end
