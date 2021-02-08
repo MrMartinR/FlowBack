bebfc432d2c90c51c13b111e8b7c9135fa3b13d8
muhammad ebeid <muhammed.ebeid@gmail.com>
2021-02-08 12:24:08 +0200

    add error serializer

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -1,5 +1,11 @@
-module ErrorSerializer
-  def self.serialize(object)
+class ErrorSerializer
+  attr_reader :object
+
+  def initialize(object)
+    @object = object
+  end
+
+  def serialize
     object.errors.messages.each_with_object({ errors: [] }) do |(field, errors), result|
       errors.each do |error|
         result[:errors] << {
