586d83d5c6baacd2a8ee4716b5d0dda68b83b1fa
muhammad ebeid <muhammed.ebeid@gmail.com>
2021-02-09 23:43:04 +0200

    use memoization into current_user

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,13 +1,47 @@
 class Api::BaseController < ApplicationController
-  include DeviseTokenAuth::Concerns::SetUserByToken
-  include ExceptionHandler
   include Response
+  include ExceptionHandler
+  include DeviseTokenAuth::Concerns::SetUserByToken
 
-  before_action :set_var
+  before_action :authenticate_api_v1_user!
+  before_action :validate_data_member, except: :index
+  before_action :validate_resource_object_type, except: :index
+  before_action :validate_resource_object_identifier, except: %i[index create]
+
+  def index; end
+
+  def create; end
+
+  def current_user
+    @current_user ||= current_api_v1_user
+  end
 
   protected
 
-  def set_var
-    @user = current_api_v1_user
+  def validate_data_member
+    raise MissingDataMember if params[:data].nil?
+  end
+
+  def validate_resource_object_type
+    raise MissingOrMismatchTypeResourceObject if
+    params[:data][:type].nil? || params[:data][:type] != current_controller
+  end
+
+  def validate_resource_object_identifier
+    raise MissingIdentifierResourceObject if params[:data][:id].nil?
+  end
+
+  def current_controller
+    controller_name == 'registrations' ? 'users' : controller_name
+  end
+
+  def render_authenticate_error
+    error = {
+      status: '401',
+      detail: 'You need to sign in or sign up before continuing'
+    }
+
+    render json: { errors: [error] },
+           status: :unauthorized
   end
 end
