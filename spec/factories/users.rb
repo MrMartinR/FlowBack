7b78614744de7ca2b941b72f2e28ac1963c9c4ad
muhammad ebeid <muhammed.ebeid@gmail.com>
2021-02-10 23:46:58 +0200

    add nested factory in user to allow creating admin

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -10,5 +10,13 @@ FactoryBot.define do
 
     password { 'password' }
     password_confirmation { 'password' }
+
+    factory :admin do
+      after(:create) { |user| user.add_role(:admin) }
+    end
+
+    factory :contributor do
+      after(:create) { |user| user.add_role(:contributor) }
+    end
   end
 end
