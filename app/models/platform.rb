47e1ca0ad9d290566e8444a8e0cb0eff6ae82a00
muhammad ebeid <muhammed.ebeid@gmail.com>
2021-02-13 18:32:42 +0200

    add dependent on platform association for safe deletion

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,9 +1,9 @@
 class Platform < ApplicationRecord
   self.implicit_order_column = 'created_at'
 
-  has_many :user_platforms
-  has_many :loans
-  has_many :user_loans
-  has_many :accounts
+  has_many :user_platforms, dependent: :destroy
+  has_many :loans, dependent: :destroy
+  has_many :user_loans, dependent: :destroy
+  has_many :accounts, dependent: :destroy
   belongs_to :contact
 end
