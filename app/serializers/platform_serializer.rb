3f83d8d081704cb6583869a1ac9c5f6f3dc3bf23
Flow <hi@flowfin.tech>
2021-05-13 15:00:56 +0100

    🐛 Platform | Add Fields to Serializer  Response

--- app/serializers/platform_serializer.rb
+++ app/serializers/platform_serializer.rb
@@ -3,9 +3,6 @@ class PlatformSerializer
   #sets the type of serializer
   set_type :platform
   #The attributes needed in response
-  attributes  :contact, :status,:liquidity, :term, :min_investment, :secondary_market, :cost, :structure, :welcome_bonus, :promo, :account_category, :protection_scheme
-
-  attribute :taxes do |object|
-    object.taxes if object.has_attribute?(:taxes)
+  attributes  :contact, :invest_mode, :taxes, :profitable, :ifisa, :sm_notes, :cashflow_options, :promo_end, :category, :status, :liquidity, :term, :min_investment, :secondary_market, :cost, :structure, :welcome_bonus, :promo, :account_category, :protection_scheme
   end
 end
